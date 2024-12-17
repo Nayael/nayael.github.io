b195f0047c21260651d189ea67450adaeb8de98c
Nicolas Vannier <nicolas.vannier@syneto.eu>
2024-12-17 20:43:41 +0200

    Initial commit: test project

---
+++ precache-manifest.fd38cfa30f9108c617b95981e2d2d9e7.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bee80211e5e6355b291c1516e1a3e29f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a46903d3f2528a008b52",
+    "url": "./static/css/2.36da85ed.chunk.css"
+  },
+  {
+    "revision": "aab3e028ca74d8e8abb4",
+    "url": "./static/css/main.8cb09ad2.chunk.css"
+  },
+  {
+    "revision": "a46903d3f2528a008b52",
+    "url": "./static/js/2.79454f0b.chunk.js"
+  },
+  {
+    "revision": "2abb8720697128fb7f115bc016550052",
+    "url": "./static/js/2.79454f0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aab3e028ca74d8e8abb4",
+    "url": "./static/js/main.96f514c5.chunk.js"
+  },
+  {
+    "revision": "1ce78816ef6070350a8a",
+    "url": "./static/js/runtime-main.ea372306.js"
+  },
+  {
+    "revision": "2fb1c1761cb170daf4623a729dca9cfc",
+    "url": "./static/media/Lato-Black.2fb1c176.ttf"
+  },
+  {
+    "revision": "6b7c634be58b6511ca3716fbc7f9058f",
+    "url": "./static/media/Lato-Black.6b7c634b.eot"
+  },
+  {
+    "revision": "7f72bebc4e932b19d5c350c99a3e8741",
+    "url": "./static/media/Lato-Black.7f72bebc.woff"
+  },
+  {
+    "revision": "9ce7573963d90a47821c440e28e1ac74",
+    "url": "./static/media/Lato-Black.9ce75739.woff2"
+  },
+  {
+    "revision": "371e49bc1fe5b9d3e35c45ce9ef8cb7f",
+    "url": "./static/media/Lato-Regular.371e49bc.eot"
+  },
+  {
+    "revision": "503c08c75f1dde3861f390cdfc2a207a",
+    "url": "./static/media/Lato-Regular.503c08c7.ttf"
+  },
+  {
+    "revision": "d8b947261b6148def606ea99cb76b596",
+    "url": "./static/media/Lato-Regular.d8b94726.woff2"
+  },
+  {
+    "revision": "fc060edcf4327a5f9a09f0503aa49cf6",
+    "url": "./static/media/Lato-Regular.fc060edc.woff"
+  },
+  {
+    "revision": "01b151b1309440b304051bbac1ca554f",
+    "url": "./static/media/Raleway-Black.01b151b1.ttf"
+  },
+  {
+    "revision": "1fd77413258e1dcd8317deb2cd590383",
+    "url": "./static/media/Raleway-Black.1fd77413.woff2"
+  },
+  {
+    "revision": "83a1cad6cef383ce4fb2a50f666625a1",
+    "url": "./static/media/Raleway-Black.83a1cad6.eot"
+  },
+  {
+    "revision": "afdbbedeffc50780207236686deec3b3",
+    "url": "./static/media/Raleway-Black.afdbbede.woff"
+  },
+  {
+    "revision": "62f6d7f33e17999f09e20cb54a0ad520",
+    "url": "./static/media/Raleway-Bold.62f6d7f3.eot"
+  },
+  {
+    "revision": "6407f3371439ef6d8acf0b0a606110e9",
+    "url": "./static/media/Raleway-Bold.6407f337.woff"
+  },
+  {
+    "revision": "69504b2d45f9ec91d29b57e0ab224118",
+    "url": "./static/media/Raleway-Bold.69504b2d.woff2"
+  },
+  {
+    "revision": "af5c97940efb437e4db7352da97bb3d0",
+    "url": "./static/media/Raleway-Bold.af5c9794.ttf"
+  },
+  {
+    "revision": "9c6035f5f6807fe9e20229d5a3bfd88b",
+    "url": "./static/media/Raleway-Medium.9c6035f5.woff2"
+  },
+  {
+    "revision": "d35a88756482cfc03267c8c8b072e45d",
+    "url": "./static/media/Raleway-Medium.d35a8875.ttf"
+  },
+  {
+    "revision": "d689769a441a55a619e4dd5d5ece7221",
+    "url": "./static/media/Raleway-Medium.d689769a.eot"
+  },
+  {
+    "revision": "e97168b381188a7fa98a05fe7d224556",
+    "url": "./static/media/Raleway-Medium.e97168b3.woff"
+  },
+  {
+    "revision": "145cf3f6f532d9b61cfefcd1e6d3f1ff",
+    "url": "./static/media/Raleway-Regular.145cf3f6.woff"
+  },
+  {
+    "revision": "3f13c93fa79af5552240264d03cb2556",
+    "url": "./static/media/Raleway-Regular.3f13c93f.woff2"
+  },
+  {
+    "revision": "4c4140038adbd42967a13014fb3f9d31",
+    "url": "./static/media/Raleway-Regular.4c414003.eot"
+  },
+  {
+    "revision": "f4b782bda9e0d7eb6ba140ca9d9c155d",
+    "url": "./static/media/Raleway-Regular.f4b782bd.ttf"
+  },
+  {
+    "revision": "2091f3335ffc92c88138854ec750e8ce",
+    "url": "./static/media/Raleway-SemiBold.2091f333.eot"
+  },
+  {
+    "revision": "477852ebb27a68c83445b11073205ffd",
+    "url": "./static/media/Raleway-SemiBold.477852eb.woff"
+  },
+  {
+    "revision": "934dfaef63ef95853a20431732d73a22",
+    "url": "./static/media/Raleway-SemiBold.934dfaef.ttf"
+  },
+  {
+    "revision": "a8c3eef9da664f6a36ab5b853b7cc5ae",
+    "url": "./static/media/Raleway-SemiBold.a8c3eef9.woff2"
+  }
+]);
